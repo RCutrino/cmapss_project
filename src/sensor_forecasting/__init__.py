2ab6ca4601e732a854a773187b27817b0d00805d
RCutrino <127293425+RCutrino@users.noreply.github.com>
2026-05-16 20:01:37 +0200

    Update __init__.py

--- src/sensor_forecasting/__init__.py
+++ src/sensor_forecasting/__init__.py
@@ -1,7 +1,7 @@
 from .metrics import evaluate_forecast
 from .models import build_sensor_lstm
+from .preprocessing import add_forecast_targets
 
-__all__ = [
-    'evaluate_forecast',
-    'build_sensor_lstm',
-]
+__all__ = ['evaluate_forecast',
+           'build_sensor_lstm',
+           'add_forecast_targets']
