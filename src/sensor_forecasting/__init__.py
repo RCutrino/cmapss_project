78565a440ead6d2078fd05f577f7038172f21544
RCutrino <127293425+RCutrino@users.noreply.github.com>
2026-05-25 11:05:52 +0200

    Update __init__.py

--- src/sensor_forecasting/__init__.py
+++ src/sensor_forecasting/__init__.py
@@ -1,7 +1,9 @@
-from .metrics import evaluate_forecast
+from .metrics import evaluate_forecast, DelayedEarlyStopping
 from .models import build_sensor_lstm
-from .preprocessing import add_forecast_targets
+from .preprocessing import add_forecast_targets, make_forecast_sequences
 
 __all__ = ['evaluate_forecast',
            'build_sensor_lstm',
-           'add_forecast_targets']
+           'add_forecast_targets',
+           'make_forecast_sequences',
+           'DelayedEarlyStopping]
