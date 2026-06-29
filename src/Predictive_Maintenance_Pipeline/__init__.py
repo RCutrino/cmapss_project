03e22e1a2cf506311837dbec70eed7132fe2ca3f
RCutrino <roberto.cutrino@gmail.com>
2026-06-29 10:34:26 +0000

    Aggiornamento da Colab

--- src/Predictive_Maintenance_Pipeline/__init__.py
+++ src/Predictive_Maintenance_Pipeline/__init__.py
@@ -1,7 +1,8 @@
-from .processing import make_sf_sequences, make_rul_sequences,
+from .processing import (make_sf_sequences, make_rul_sequences,
                         forecast_anomaly_score, isolation_forest_score, 
                         autoencoder_score, combine_anomaly_scores, 
-                        assign_alert, true_alert, dominant_signal
+                        assign_alert, true_alert, dominant_signal,
+                        predict_rul)
 
 __all__ = ['make_sf_sequences', 
            'make_rul_sequences',
@@ -9,6 +10,7 @@ __all__ = ['make_sf_sequences',
            'isolation_forest_score',
            'autoencoder_score', 
            'combine_anomaly_scores',
+           'predict_rul',
            'assign_alert',
            'true_alert',
            'dominant_signal']
